71c8a4583bf62663595787111d0cd3034bda0d23
Liam Greenway <ApplePiePc@outlook.com>
2025-07-03 20:16:07 -0400

    E

---
+++ TestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using ADTP;
+
+namespace TestApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        new Thread(ClientThread).Start();
+        var listener = new AdtpListener(2025);
+        var client = listener.AcceptConnectionSecure();
+        var request = client.Recieve();
+        Console.WriteLine(request.Build());
+    }
+
+    static void ClientThread()
+    {
+        var client = new AdtpClient();
+        client.ConnectSecure("127.0.0.1", 2025);
+        client.Send(new RequestBuilder().SetContent("Hello World"));
+    }
+}
